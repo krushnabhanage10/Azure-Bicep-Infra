9cc311eca5ed30087ff3eb0db0ee3d42be9f7f1a
Krushna Bhanage <46320727+krushnabhanage10@users.noreply.github.com>
2023-10-28 18:36:31 +0530

    Create Nginx-Ingress-for-AKS.sh

---
+++ Ingress/Nginx-Ingress-for-AKS.sh
@@ -0,0 +1,7 @@
+kubectl create ns ingress
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --create-namespace \
+  --namespace ingress \
+  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz
